388e073a28ff5d21bee02c22118fd812ed310d6d
Mbark Hend <mhend@c2r8p1.1337.ma>
2026-01-05 01:19:25 +0100

    pars&instr

--- rev_rotate.c
+++ rev_rotate.c
@@ -6,7 +6,7 @@
 /*   By: mhend <mhend@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 00:52:35 by mhend             #+#    #+#             */
-/*   Updated: 2026/01/05 01:05:28 by mhend            ###   ########.fr       */
+/*   Updated: 2026/01/05 01:17:16 by mhend            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,29 @@ static void     rev_rotate(t_stack **a)
     lastt = last(*a);
     (*a)->prev = lastt;
     lastt->next = *a;
-    lastt->prev = NULL;
+    lastt->prev->next = NULL;
     *a = lastt;
     (*a)->prev = NULL;  
+}
+
+void    rra(t_stack **a,  int print)
+{
+    rev_rotate(a);
+    if (print)
+        write(1, "rra\n", 4);
+}
+
+void    rrb(t_stack **b,  int print)
+{
+    rev_rotate(b);
+    if (print)
+        write(1, "rrb\n", 4);
+}
+
+void    rrr(t_stack **a, t_stack **b,  int print)
+{
+    rev_rotate(a);
+    rev_rotate(b);
+    if (print)
+        write(1, "rrr\n", 4);
 }
